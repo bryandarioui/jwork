531a312e1686b845cd7835f5888c76649b739732
bryandarioui <bryan.dario@ui.ac.id>
2021-05-20 18:11:55 +0700

    Modul 8 - Case Study

--- src/main/java/BryanDarioLesmana/jwork/Jwork.java
+++ src/main/java/BryanDarioLesmana/jwork/Jwork.java
@@ -6,21 +6,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Jwork {
 
         public static void main(String[] args) {
-            Location lct1 = new Location("Jakarta", "Jakpus", "LOL");
-            Location lct2 = new Location("Bandung", "Bandung Barat", "Mana ada");
-            Location lct3 = new Location("Serang", "Seoul", ":O");
+            Location location1 = new Location("Jakarta", "Jakarta Pusat", "LOL");
+            Location location2 = new Location("Bandung", "Bandung", "Mana ada");
+            Location location3 = new Location("Tanggerang", "Serpong", ":O");
 
-            DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId()+1, "Dimas", "dimasr9@gmail.com", "08568406688", lct1));
-            DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId()+1, "Radhitya", "radhitya9@gmail.com", "08568806688", lct2));
-            DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId()+1, "DmsRdhty", "dimasradhitya9@gmail.com", "08567906688", lct3));
+            DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId()+1, "Bryan", "bryan@gmail.com", "083123123", location1));
+            DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId()+1, "Dario", "bryan2@gmail.com", "08671232131", location2));
+            DatabaseRecruiter.addRecruiter(new Recruiter(DatabaseRecruiter.getLastId()+1, "bryandario", "bryandario@gmail.com", "08218312736", location3));
 
             try{
-                DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1, "Bryan1", DatabaseRecruiter.getRecruiterById(1), 80000, JobCategory.DataAnalyst));
+                DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1, "Bryan1", DatabaseRecruiter.getRecruiterById(1), 80000, JobCategory.WebDeveloper));
             }catch (RecruiterNotFoundException error){
                 System.out.println(error.getMessage());
             }
             try{
-                DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1, "Bryan2", DatabaseRecruiter.getRecruiterById(2), 80000, JobCategory.DataAnalyst));
+                DatabaseJob.addJob(new Job(DatabaseJob.getLastId()+1, "Bryan2", DatabaseRecruiter.getRecruiterById(2), 80000, JobCategory.WebDeveloper));
             }catch (RecruiterNotFoundException error){
                 System.out.println(error.getMessage());
             }
