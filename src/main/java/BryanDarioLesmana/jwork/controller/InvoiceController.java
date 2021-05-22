d31b6aa64fea727822d8274dbc8b4f392f12d279
bryandarioui <bryan.dario@ui.ac.id>
2021-05-22 09:45:07 +0700

    Modul 8 - Case Study

---
+++ src/main/java/BryanDarioLesmana/jwork/controller/InvoiceController.java
@@ -0,0 +1,104 @@
+package BryanDarioLesmana.jwork.controller;
+
+import BryanDarioLesmana.jwork.*;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RequestMapping("/invoice")
+@RestController
+public class InvoiceController {
+
+    @RequestMapping("")
+    public ArrayList<Invoice> getAllInvoice(){
+        return DatabaseInvoice.getInvoiceDatabase();
+    }
+    @RequestMapping("/{id}")
+    public Invoice getInvoiceById(@PathVariable int id) {
+        Invoice invoice = null;
+        try {
+            invoice = DatabaseInvoice.getInvoiceById(id);
+        } catch (InvoiceNotFoundException e) {
+            e.getMessage();
+            return null;
+        }
+        return invoice;
+    }
+
+    @RequestMapping(value = "/jobseeker/{jobseekerId}", method = RequestMethod.GET)
+    public ArrayList<Invoice> getInvoiceByJobseeker(@PathVariable int jobseekerId) {
+        return DatabaseInvoice.getInvoiceByJobseeker(jobseekerId);
+    }
+
+    @RequestMapping(value = "/invoiceStatus/{id}", method = RequestMethod.PUT)
+    public Invoice changeInvoiceStatus(@RequestParam(value="id") int id,
+                                       @RequestParam(value="invoiceStatus") InvoiceStatus invoiceStatus) throws InvoiceNotFoundException {
+        DatabaseInvoice.changeInvoiceStatus(id, invoiceStatus);
+        Invoice invoice = DatabaseInvoice.getInvoiceById(id);
+        return invoice;
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public Boolean removeInvoice(@RequestParam(value = "id") int id){
+        try{
+            DatabaseInvoice.removeInvoice(id);
+        } catch (InvoiceNotFoundException e){
+            e.getMessage();
+            return false;
+        }
+        return true;
+    }
+
+
+    @RequestMapping(value = "/createBankPayment", method = RequestMethod.POST)
+    public Invoice addBankPayment(@RequestParam(value = "jobIdList") ArrayList<Integer> jobIdList,
+                                  @RequestParam(value = "jobseekerId") int jobseekerId,
+                                  @RequestParam(value = "adminId") int adminFee){
+        ArrayList<Job> job = new ArrayList<>();
+        for (int jb : jobIdList) {
+            try {
+                job.add(DatabaseJob.getJobById(jb);
+            } catch (JobNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        try {
+            Invoice invoice = new BankPayment(DatabaseInvoice.getLastId()+1, job,
+                    DatabaseJobseeker.getJobseekerById(jobseekerId), adminFee);
+            DatabaseInvoice.addInvoice(invoice);
+            invoice.setTotalFee();
+            return invoice;
+        } catch (JobSeekerNotFoundException | OngoingInvoiceAlreadyExistsException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    @RequestMapping(value = "/createEWalletPayment", method = RequestMethod.POST)
+    public Invoice addBankPayment(@RequestParam(value = "jobIdList") ArrayList<Integer> jobIdList,
+                                  @RequestParam(value = "jobseekerId") int jobseekerId,
+                                  @RequestParam(value = "referralCode") Bonus referralCode)
+    {
+        try{
+            ArrayList<Job> jobs = new ArrayList<Job>();
+            for (Integer id : jobIdList){
+                Job job = DatabaseJob.getJobById(id);
+                jobs.add(job);
+            }
+            Jobseeker jobseeker = DatabaseJobseeker.getJobseekerById(jobseekerId);
+            EwalletPayment ep = new EwalletPayment(DatabaseInvoice.getLastId() + 1, jobs, jobseeker, referralCode);
+            DatabaseInvoice.addInvoice(ep);
+            return ep;
+        } catch (JobSeekerNotFoundException e){
+            System.out.println(e.getMessage());
+        } catch (JobNotFoundException e){
+            System.out.println(e.getMessage());
+        } catch (OngoingInvoiceAlreadyExistsException e){
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+
+
+}
+
